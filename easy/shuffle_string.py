579050372ac26d8b0eee1457512254ecac074c38
unknown <im.chandankm@gmail.com>
2021-04-21 17:04:52 -0400

    adding shuffle string script

---
+++ easy/shuffle_string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        m=''
+        for i in sorted(zip(indices,s)):
+           m=m+(i[1])
+        return m
+print("Done")
